04e338139fd54bb67f1aed33afd32ce9dd18aa06
Scott Ashton <scott@writeaboutdragons.com>
2014-02-11 09:32:39 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Angular Custom Bind
+------------
+
+Angular custom bind is a tool built to allow users to define custom delimiters in
+the view that only update on specified broadcasts, with an optional scope
---
+++ angular-custom-bind.js
@@ -0,0 +1,72 @@
+angular.module("customBind", []).factory('ProjectFactory', ['$filter', '$http', 'LawnchairFactory', '$rootScope',
+    function($filter, $http, LawnchairFactory, $rootScope) {
+
+
+        return {
+            setListener: function(delim1, delim2, trigger, scope){
+                var boundScope = scope ? scope : $rootScope;
+                boundScope.$on(trigger, function(){
+
+                    var startTime = (new Date()).getTime();
+                    walkDOM(angular.element('[ng-app]')[0], function(node){
+                        var scope;
+                        var patt = new RegExp(delim1 + "(.*?)" + delim2, "g");
+
+                        //Look for bindings in text nodes
+                        if(node.wholeText && node.wholeText.trim().length > 0){
+                            scope = angular.element(node).scope();
+                            var changed = false;
+                            node.textContent = node.textContent.replace(patt, function(outer, inner, index){
+                                var a = node.textContent;
+                                var replaceValue = scope.$eval(inner);
+                                replaceValue = replaceValue ? replaceValue : "";
+                                changed = true;
+                                return replaceValue;
+                            })
+                        }
+
+                        //And in dom attributes (strings only)
+                        if(node.attributes && node.attributes.length != 0){
+                            scope = angular.element(node).scope();
+
+                            for(var i = 0; i < node.attributes.length; i++){
+                                var thisAttr = node.attributes[i].nodeValue;
+
+                                if(thisAttr && thisAttr.length > 0 && thisAttr.indexOf("[[") != -1){ //This is a much quicker check than the replace check
+                                    node.attributes[i].nodeValue = thisAttr.replace(patt, function(outer, inner, index){
+                                        var replaceValue = scope.$eval(inner);
+                                        replaceValue = replaceValue ? replaceValue : "";
+                                        return replaceValue;
+                                    })
+                                }
+                            }
+                        }
+
+                    })
+
+                    var endTime = (new Date()).getTime();
+
+                    console.debug(endTime - startTime)
+                });
+
+
+            }
+        };
+
+        //Todo A DOM Tree walker might be faster than this.
+        function walkDOM(node,func) {
+            func(node);
+            node = node.firstChild;
+            while(node) {
+                walkDOM(node,func);
+                node = node.nextSibling;
+            }
+
+        };
+
+
+
+    }])
+
+
+
